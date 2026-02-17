a9a356ca59bf6af52c3bed58248458cb1d823597
11NP <navaneethp672@gmail.com>
2026-02-18 00:11:16 +0530

    Fixed MODEL

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("video");
 
 let model;
-let warningAudio = new Audio("warning.mp3");
 
 async function loadModel() {
   model = await cocoSsd.load();
+  console.log("MODEL LOADED");
 }
 
 startBtn.addEventListener("click", async () => {
+
+  console.log("BUTTON CLICKED");
 
   startBtn.style.display = "none";
 
@@ -18,19 +20,18 @@ startBtn.addEventListener("click", async () => {
 
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "environment" }
+      video: true
     });
 
     video.srcObject = stream;
     await video.play();
 
-    video.addEventListener("loadeddata", () => {
-      detectFrame();
-    });
+    console.log("VIDEO READY STATE:", video.readyState);
+
+    detectFrame();
 
   } catch (error) {
     console.error("Camera error:", error);
-    alert("Camera access failed. Check browser permissions.");
   }
 });
 
@@ -41,17 +42,4 @@ async function detectFrame() {
   console.log("Predictions:", predictions);
 
   requestAnimationFrame(detectFrame);
-}
-
-
-let lastAlertTime = 0;
-
-function triggerWarning() {
-  const now = Date.now();
-
-  // Prevent audio spam
-  if (now - lastAlertTime > 2000) {
-    warningAudio.play();
-    lastAlertTime = now;
-  }
 }
